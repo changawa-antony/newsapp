7de6cd40b3b0e2f5f70fb91df2e355c629cb6b24
changawa-antony <antony.changawa@student.moringaschool.com>
2021-06-08 19:21:40 +0300

    added block content to index.html

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/moringa/Documents/moringa-core-projects/Watchlist/virtual"
+set -gx VIRTUAL_ENV "/home/moringa/Documents/moringa-core-projects/newsapp/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
